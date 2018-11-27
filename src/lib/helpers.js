01be2d782e9328a6709b03a705d6c15b27802dd8
mariodawg <44001792+mariodawg@users.noreply.github.com>
2018-11-27 16:29:44 +0000

    Setti upp Rollup og Babel

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
 export function empty(element) {
+  if (element === null) return;
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
